ab0bf2362cd372670b37cfce0af80d5af2029379
telmo-bidart <telmobidart@gmail.com>
2023-03-12 10:31:41 +0000

    feat: add theme settings for light and dark mode

--- src/theme.js
+++ src/theme.js
@@ -128,24 +128,82 @@ export const themeSettings = (mode) => {
   return {
     palette: {
       mode: mode,
-      ...(mode === 'dark' 
-      ? {
-        primary: {
-          main: colors.primary[500],
-        },
-        secondary: { main: colors.greenAccent[500]},
-        neutral: {
-          dark: colors.gray[700],
-          main: colors.gray[500],
-          light: colors.gray[100]
-        },
-        background: {
-          default: colors.primary[500],
-        }
-      } : {
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: { main: colors.greenAccent[500] },
+            neutral: {
+              dark: colors.gray[700],
+              main: colors.gray[500],
+              light: colors.gray[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: { main: colors.greenAccent[500] },
+            neutral: {
+              dark: colors.gray[700],
+              main: colors.gray[500],
+              light: colors.gray[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
 
-      }
-      )
-    }
-  }
-}
+// context for color mode
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () =>
+      setMode((prev) => (prev === "light" ? "dark" : "light")),
+  }));
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+};
