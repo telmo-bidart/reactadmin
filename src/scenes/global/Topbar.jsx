0233db13362e7180cb493697e3de16baccb8610e
telmo-bidart <telmobidart@gmail.com>
2023-03-15 12:31:57 +0000

    feat: add topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,18 @@
-const Topbar = () => {
-  return <div>Topbar
-  
-  </div>
-}
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => { 
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return <div>Topbar</div>;
+};
 
 export default Topbar;
