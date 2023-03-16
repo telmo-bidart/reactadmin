57627558fb1743d1d69ab7784f180ebecf5fa5d3
telmo-bidart <telmobidart@gmail.com>
2023-03-16 11:53:09 +0000

    feat: add  search bar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -12,7 +12,17 @@ import SearchIcon from "@mui/icons-material/Search";
 const Topbar = () => { 
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  return <div>Topbar</div>;
+  const colorMore = useContext(ColorModeContext);
+
+  return (<box display="flex" justifyContent="space-between" p={2}>
+    {/*search bar */}
+    <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      <InputBase sx={{ ml: 2, flex: 1}} placeholder="Search" />
+    <IconButton type="button" sx={{ p: 1 }}>
+      <SearchIcon />
+    </IconButton>
+    </Box>
+  </box>);
 };
 
 export default Topbar;
